fe417806226243bb06ca1c03a85bb2f5c16bb3a4
AngelNifla <mnifla@unsa.edu.pe>
2026-04-14 18:39:18 -0500

    V1 de la App: agregando icono a las ventanas

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from ui.login import Login
+from utils import app_icon
 
 def main():
     app = QApplication(sys.argv)
+
+    # ⭐ ICONO GLOBAL DE TODA LA APLICACIÓN
+    app.setWindowIcon(app_icon())
 
     login = Login()
     login.show()
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import sys
 import os
+from PySide6.QtGui import QIcon
+from utils import ruta_recurso
 
 def ruta_recurso(rel_path):
     """
@@ -12,3 +14,19 @@ def ruta_recurso(rel_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, rel_path)
+
+
+def app_icon():
+    """
+    Icono global de la aplicación
+    """
+    ruta = ruta_recurso("img/logo.ico")
+
+    self.setStyleSheet(f"""
+        QWidget {{
+            background-image: url({ruta});
+            background-repeat: no-repeat;
+            background-position: center;
+        }}
+    """)
+    return QIcon(ruta_recurso(ruta))
